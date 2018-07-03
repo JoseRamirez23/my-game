7d62e365eec2947c7af9fcea4d67c18979760baa
Jose Ramirez <shadow661199@gmail.com>
2018-07-02 17:35:09 -0700

    first part of my game but i am still not finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  
+  number:number=0;
+  timer:any;
+  button:boolean=false;
+
+  start(){
+    this.button = true;
+    this.timer = setInterval(() => {
+      this.number += 1;
+    },1000);
+    
+  }
+
+  stop(){
+    this.button=false;
+    clearInterval(this.timer);
+
+    let newNumber = this.number;
+  }
+ ngOnInit() {
+  console.log(this.number);
+
+
+ }
+
 }
